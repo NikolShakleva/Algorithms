2cc5f9a49ce874f1c88043150b2eb904376bc3d3
emsk <emsk@itu.dk>
2020-12-15 17:15:05 +0100

    stack comment

--- Part2/CorrectnessTest.java
+++ Part2/CorrectnessTest.java
@@ -1,17 +1,23 @@
 import java.util.Arrays;
 
+/**
+ * 
+ *  run with a larger stack
+ *  java -Xss2g CorrectnessTest
+ */
+
 public class CorrectnessTest {
    private static String [] algo = {"QuickSortClassic","DualPivotQuickSort","ThreePivotQuickSort"};
-   private static String [] twoPivot = {"DualPivotQuickSort",  "DualPivotQuickSortWrong","ThreePivotQuickSort", "ThreePivotQuickSortWrong"};
+   //private static String [] algo = {"DualPivotQuickSort",  "DualPivotQuickSortWrong","ThreePivotQuickSort", "ThreePivotQuickSortWrong"};
    private static String [] mode = {"increasing", "decreasing", "same", "random", "equal", "semi-sorted"};
    private static int [] seed = Seed.createSeed(1234);
-   private static int N = 10_000;
+   private static int N = 50_000;
    
 
     public static String test (int [] inputArray, String algorithm) {
 
-        //  if(algorithm.equals("QuickSortClassic")) {QuickSortClassic.sort(inputArray, 0, inputArray.length-1);}
-          if(algorithm.equals("DualPivotQuickSort")) {DualPivotQuickSort.sort(inputArray, 0, inputArray.length-1);}
+         if(algorithm.equals("QuickSortClassic")) {QuickSortClassic.sort(inputArray, 0, inputArray.length-1);}
+         else if(algorithm.equals("DualPivotQuickSort")) {DualPivotQuickSort.sort(inputArray, 0, inputArray.length-1);}
          else if(algorithm.equals("DualPivotQuickSortWrong")) {DualPivotQuickSortWrong.sort(inputArray, 0, inputArray.length-1);}
          else if(algorithm.equals("ThreePivotQuickSort")) {ThreePivotQuickSort.sort(inputArray, 0, inputArray.length-1);}
          else if(algorithm.equals("ThreePivotQuickSortWrong")) {ThreePivotQuickSortWrong.sort(inputArray, 0, inputArray.length-1);}
@@ -48,12 +54,12 @@ public class CorrectnessTest {
     public static void main(String[] args) {
         for (int i = 0; i < mode.length; i++) {
             for(int  l = 0; l< seed.length; l++) {
-                 for(int j = 0; j < twoPivot.length; j++) {
+                 for(int j = 0; j < algo.length; j++) {
                      int [] inputArray = Producer.generate(mode[i], N, seed[l]);
-                    //  System.out.println("Correctness test for: " + twoPivot[j] + " mode " + mode[i] + " seed: " + seed[l]);
-                     String value =  test(inputArray, twoPivot[j]);
+                    //  System.out.println("Correctness test for: " + algo[j] + " mode " + mode[i] + " seed: " + seed[l]);
+                     String value =  test(inputArray, algo[j]);
                      if(value.equals("FAILURE")) {
-                        System.out.println("FAILURE. " + twoPivot[j]  + " is not correct for mode: " + mode[i] + " for seed: " + seed[l]);
+                        System.out.println("FAILURE. " + algo[j]  + " is not correct for mode: " + mode[i] + " for seed: " + seed[l]);
                         System.out.println("--------------------------------------------------------------------------------------------");
                         System.out.println();
                      }
